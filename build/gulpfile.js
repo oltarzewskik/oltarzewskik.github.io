7097bdece39110d28a4c1b884b914bbb8b119be6
Karol <2004karol@gmail.com>
2018-12-17 20:02:39 +0100

    init

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -1,9 +1,8 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 
-"use strict";
-gulp.task('sass', function () {
-    gulp.src('sass/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('../assets/css/'));
-})
+gulp.task('sass', function(){
+    return gulp.src('sass/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./../assets/css/'));
+});
